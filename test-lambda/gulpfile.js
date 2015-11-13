05d7d3ab49d2cadc900c927da4ba245911fd0075
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2015-11-13 16:48:30 +0100

    prerelease

--- test-lambda/gulpfile.js
+++ test-lambda/gulpfile.js
@@ -2,11 +2,12 @@
 
 require('colors');
 var gulp = require('gulp'),
-    del = require('del'),
+    data = require('gulp-data'),
     fs = require('fs'),
     path = require('path'),
     zip = require('gulp-zip'),
     uglify = require('gulp-uglify'),
+    filter = require('gulp-filter'),
     inquirer = require('inquirer'),
     AWS = require('aws-sdk');
 
@@ -16,28 +17,12 @@ try {
     lambda_config = null;
 }
 
-gulp.task("default", ['configure'], function(){
+gulp.task("default", ['configure']);
 
-});
-
-gulp.task('clean', function(){
-    del.sync(src[i]+'/dist', {force:true});
-});
-
-gulp.task('zip', function(next){
-    gulp.src(["/src/**/*"])
-        .pipe(zip(lambda_config.ConfigOptions.FunctionName+".zip"))
-        .pipe(gulp.dest(src[i]+"/dist"))
-});
-
-gulp.task('build', function(next){
-
-});
-
-gulp.task('configure', function(){
+gulp.task('configure', function(next){
     inquirer.prompt([
         {type: 'input', name: 'FunctionName', message: 'Function name:', default: lambda_config? lambda_config.ConfigOptions.FunctionName:'my-lambda'},
-        {type: 'input', name: 'Region', message: 'Region:',  default: lambda_config? lambda_config.ConfigOptions.Region:"eu-west-1"},
+        {type: 'input', name: 'Region', message: 'Region:',  default: lambda_config? lambda_config.Region:"eu-west-1"},
         {type: 'input', name: 'Description', message: 'Description:',  default: lambda_config? lambda_config.ConfigOptions.Description:null},
         {type: 'input', name: 'Role', message: 'Role arn:',  default: lambda_config? lambda_config.ConfigOptions.Role:null},
         {type: 'input', name: 'Handler', message: 'Handler:',  default: lambda_config? lambda_config.ConfigOptions.Handler:"index.js"},
@@ -46,14 +31,14 @@ gulp.task('configure', function(){
     ], function (config_answers) {
         lambda_config = {
             Region: config_answers.Region,
+            Runtime: 'nodejs',
             ConfigOptions: {
                 FunctionName: config_answers.FunctionName,
                 Description: config_answers.Description,
                 Role: config_answers.Role,
                 Handler: config_answers.Handler,
                 MemorySize: config_answers.MemorySize,
-                Timeout: config_answers.Timeout,
-                Runtime: 'nodejs'
+                Timeout: config_answers.Timeout
             }
         };
 
@@ -61,62 +46,72 @@ gulp.task('configure', function(){
             if (err) console.log(err.red);
             else console.log('\n',lambda_config,'\n\n',"Lambda configuration saved.".green);
         });
+
+        next();
     });
 });
 
-gulp.task("create", function(){
+gulp.task("create", function(next){
     checkConfig();
-    var params = lambda_config.ConfigOptions;
-    var lambda = new AWS.Lambda({ region: lambda_config.Region });
-    params.Code = {
-        ZipFile: fs.readFileSync(src[i]+"/dist/"+config[i].FunctionName+".zip")
-    };
 
-    lambda.createFunction(params, function(err, data) {
-        if (err){
-            console.log("FAILED".red, "-", err.message.red);
-            console.log(err, err.stack);
-        }
-        else console.log("SUCCESS".green, "- lambda", data.FunctionName.cyan, "created");
+    buildLambdaZip(function(zip){
+        var params = lambda_config.ConfigOptions;
+        var lambda = new AWS.Lambda({ region: lambda_config.Region });
+        params.Code = { ZipFile: zip };
+        params.Runtime = lambda_config.Runtime;
+
+        lambda.createFunction(params, function(err, data) {
+            if (err){
+                console.log("FAILED".red, "-", err.message.red);
+                console.log(err);
+            }
+            else console.log("SUCCESS".green, "- lambda", data.FunctionName.cyan, "created");
+            next();
+        });
     });
 });
 
 gulp.task("update", ["update-config","update-code"]);
 
-gulp.task("update-config", function(){
+gulp.task("update-config", function(next){
     checkConfig();
     var lambda = new AWS.Lambda({ region: lambda_config.Region });
 
     lambda.updateFunctionConfiguration(lambda_config.ConfigOptions, function(err, data) {
         if (err){
             console.log("FAILED".red, "-", err.message.red);
-            console.log(err, err.stack);
+            console.log(err);
         }
         else {
             console.log("SUCCESS".green, "- lambda", data.FunctionName.cyan, "config updated");
             console.log(data);
         }
+        next();
     });
 });
 
-gulp.task("update-code", function(){
-    var params = {
-        FunctionName: lambda_config.ConfigOptions.FunctionName,
-        ZipFile: fs.readFileSync(src[i]+"/dist/"+config[i].FunctionName+".zip")
-    };
-    lambda.updateFunctionCode(params, function(err, data) {
-        if (err){
-            console.log("FAILED".red, "-", err.message.red);
-            console.log(err, err.stack);
-        }
-        else {
-            console.log("SUCCESS".green, "- lambda", data.FunctionName.cyan, "code updated");
-            console.log(data);
-        }
+gulp.task("update-code", function(next){
+    buildLambdaZip(function(zip) {
+        var lambda = new AWS.Lambda({ region: lambda_config.Region });
+        var params = {
+            FunctionName: lambda_config.ConfigOptions.FunctionName,
+            ZipFile: zip
+        };
+        lambda.updateFunctionCode(params, function(err, data) {
+            if (err){
+                console.log("FAILED".red, "-", err.message.red);
+                console.log(err);
+            }
+            else {
+                console.log("SUCCESS".green, "- lambda", data.FunctionName.cyan, "code updated");
+                console.log(data);
+            }
+            next();
+        });
     });
 });
 
-gulp.task('delete', function(){
+gulp.task('delete',function(next){
     checkConfig();
     var lambda = new AWS.Lambda({ region: lambda_config.Region });
     lambda.deleteFunction({ FunctionName: lambda_config.ConfigOptions.FunctionName }, function(err) {
@@ -124,9 +119,9 @@ gulp.task('delete', function(){
             console.log("FAILED".red, "-", err.message.red);
             console.log(err);
         }
-        else {
-            console.log("SUCCESS".green, "- lambda deleted");
-        }
+        else console.log("SUCCESS".green, "- lambda deleted");
+
+        next();
     });
 });
 
@@ -135,4 +130,16 @@ function checkConfig(){
         console.log("lambda_config.json".red, "not found!", '\nRun "' + "gulp configure".cyan + '" task to set up your lambda details.' )
         process.exit();
     }
+}
+
+function buildLambdaZip(next){
+    var jsFilter = filter('**/*.js', {restore:true});
+    gulp.src("src/**/*")
+        .pipe(jsFilter)
+        .pipe(uglify())
+        .pipe(jsFilter.restore)
+        .pipe(zip(lambda_config.ConfigOptions.FunctionName+".zip"))
+        .pipe(data(function(data) {
+            next(data.contents);
+        }));
 }
